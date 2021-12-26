51ec88ee0eaed9554057ad388df0164c96962a00
tenznz <dhugkar@gmail.com>
2021-12-26 00:35:11 -0800

    Update salary field from java program

--- src/main/java/com/bridgelabz/payroll/PayrollServiceDB.java
+++ src/main/java/com/bridgelabz/payroll/PayrollServiceDB.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.payroll;
 
+import java.awt.DisplayMode;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -7,16 +8,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Enumeration;
+import java.util.Scanner;
 
 public class PayrollServiceDB {
 
-	public static void main(String[] args) {
+	Connection con = null;
+
+	public static void main(String[] args) throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 		String userName = "root";
 		String password = "root";
-		Connection con = null;
-		Statement stmt = null;
-		ResultSet res = null;
+
+		PayrollServiceDB payrollDB = new PayrollServiceDB();
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.print("Driver loaded");
@@ -24,36 +28,74 @@ public class PayrollServiceDB {
 		} catch (ClassNotFoundException e) {
 			throw new IllegalStateException("cannot find the driver in the path!", e);
 		}
-		listDriver();
+		payrollDB.listDriver();
 
 		try {
 			System.out.println("Connecting to database:" + jdbcURL);
-			con = DriverManager.getConnection(jdbcURL, userName, password);
-			System.out.println("Connection successfull! " + con);
+			payrollDB.con = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection successfull! " + payrollDB.con);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		payrollDB.OperateDB();
+
+	}
+
+	private void OperateDB() throws SQLException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1 to continue");
+		int exit = 1;
+
+		while (exit == 1) {
+			int input = sc.nextInt();
+			switch (input) {
+			case 1:
+				display();
+				break;
+			case 2:
+				update();
+				break;
+			}
+		}
+
+	}
+
+	private void update() throws SQLException {
+		String updateQuery = "update employee set salary=3000000.00 where emp_id='emp1'";
+
+		Statement stmt = null;
+		int res = 0;
 		try {
 			stmt = con.createStatement();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		String query = "Select * from Employee;";
-
-		try {
-			res = stmt.executeQuery(query);
-
-			while (res.next()) {
-				System.out.println(res.getString(1) + " " + res.getString(2) + " " + res.getString(3) + " "
-						+ res.getString(4) + " " + res.getString(5) + " " + res.getString(6) + " " + res.getString(7));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		res = stmt.executeUpdate(updateQuery);
+		if (res > 0) {
+			System.out.println("Data got inseted");
 		}
 
 	}
 
-	private static void listDriver() {
+	private void display() throws SQLException {
+		String query = "Select * from Employee;";
+		Statement stmt = null;
+		ResultSet res = null;
+		try {
+			stmt = con.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		res = stmt.executeQuery(query);
+
+		while (res.next()) {
+			System.out.println(res.getString(1) + " " + res.getString(2) + " " + res.getString(3) + " "
+					+ res.getString(4) + " " + res.getString(5) + " " + res.getString(6) + " " + res.getString(7));
+		}
+	}
+
+	private void listDriver() {
 		Enumeration<Driver> driverList = DriverManager.getDrivers();
 		while (driverList.hasMoreElements()) {
 			Driver driverClass = driverList.nextElement();
@@ -61,4 +103,5 @@ public class PayrollServiceDB {
 		}
 
 	}
+
 }
